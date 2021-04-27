8ae60b731e9fbcf83064aff7c77235de8afb8c44
點 <sh4o4hs@gmail.com>
2021-04-27 16:15:18 +0800

    update

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,7 +17,7 @@ module.exports = function(grunt) {
     DEVELOPER: 'developer',
     RELEASE:'release'
   };
-  console.log('[stuff version 6.14.0]');
+  console.log('[stuff version 6.14.1]');
   console.log(__dirname);
   grunt.file.setBase(__dirname);
   
