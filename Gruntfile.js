d04042246b6954f1278390023360f1ca9d07ba53
點 <sh4o4hs@gmail.com>
2023-03-24 10:27:48 +0800

    update

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,7 +19,7 @@ module.exports = function(grunt) {
     RELEASE:'release'
   };
   
-  console.log('[stuff version 7.6.0]');
+  console.log('[stuff version 7.7.0]');
 
   grunt.file.setBase(__dirname);
 
