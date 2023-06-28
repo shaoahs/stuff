c3a071b59b13a438f3a345139d045e354bdf6c1c
點 <sh4o4hs@gmail.com>
2023-06-28 16:50:30 +0800

    update

--- Gruntfile.js
+++ Gruntfile.js
@@ -4019,7 +4019,7 @@ module.exports = function(grunt) {
     }
 
     grunt.config.set('clean.public', JSON.parse(str));
-    if(workspace.public.useClean){
+    if(workspace.public.useClean) {
       cmdList.push(cleanName);
     }
 
@@ -4038,7 +4038,7 @@ module.exports = function(grunt) {
       });
     }
 
-    if(isFramework){
+    if(isFramework) {
       let files = workspace.public.cloneList;
       if(framework.systemjs && framework.systemjs.data && Array.isArray(framework.systemjs.data)){
         framework.systemjs.data.forEach(function (d){
@@ -4096,24 +4096,25 @@ module.exports = function(grunt) {
         cmdList.push('replace:importmap');
       }
 
-      if(pkg.currentMode === MODE.RELEASE) {
-        // if('agent' === pkg.name) {
-        //   cmdList.push('obfuscate:resource');
-        //   cmdList.push('obfuscate:vendor');
-        // }
-        cmdList.push('obfuscate:resource');
-        cmdList.push('obfuscate:vendor');
-
-        cmdList.push('compress:project');
-        cmdList.push('compress:project1');
-      }
-
       if(event && event.finish) {
         event.finish({
           mode: currentMode,
           cmdList:cmdList
         });
       }
+
+      if(pkg.currentMode === MODE.RELEASE) {
+        if('agent' === pkg.name || 'domains' === pkg.name || 'photos' === pkg.name) {
+          cmdList.push('obfuscate:resource');
+          cmdList.push('obfuscate:vendor');
+        }
+        // cmdList.push('obfuscate:resource');
+        // cmdList.push('obfuscate:vendor');
+
+        cmdList.push('compress:project');
+        cmdList.push('compress:project1');
+      }
+
     }
 
     grunt.task.run(cmdList);
---
+++ developer/agent/app/main.9.0.0.06e4065753bf3842.js
@@ -0,0 +1,1 @@
+import"./agent.3e740e2d.js";import"mithril";import"localforage";import"nuts";
